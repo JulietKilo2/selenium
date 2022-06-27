0b9d2b9a25ab797f45b7464a4e75970582e908a2
Jisub Kim <jisubkim02@gmail.com>
2022-06-27 17:49:57 +0900

    Update test_booking.js

--- test_booking.js
+++ test_booking.js
@@ -11,8 +11,8 @@ const logger = (log) => {
 
 let testLog = `log starts at: ${Date()}`;
 const localHost = "http://localhost:8080/";
-const email = process.env.CH_HELPER_EMAIL;
-const pw = process.env.CH_HELPER_PW;
+const email = process.env.MY_EMAIL;
+const pw = process.env.MY_PW;
 
 const run = async () => {
   // 1. chromedriver 경로 설정
@@ -23,94 +23,36 @@ const run = async () => {
   // 2. chrome 브라우저 빌드
   const driver = await new webdriver.Builder().forBrowser("chrome").build();
 
-  // 3. localHost
+  // 3. localHost 창 열기
   await driver.get(localHost);
   logger(`Opening up the page ${localHost}`);
 
-  // 4. deals page
   try {
+    // 4. deals page
     logger("redirect to deals page");
     await driver.get(localHost + "/deals");
     logger("success");
-  } catch (error) {
-    logger(`error: ${error}`);
-    // await driver.quit();
-    // process.exit(0);
-  }
-
-  // 5. click on deal item
-  try {
+    // 5. click on deal item
     logger("Click on deals item");
-    await driver.wait(until.elementLocated(By.css("#dealsItem-1"))).click();
+    await driver.wait(until.elementLocated(By.css("#dealsItem-0"))).click();
     logger("success");
-  } catch (error) {
-    logger(`error: ${error}`);
-    // await driver.quit();
-    // process.exit(0);
-  }
-
-  // 6. click on refund policy
-  try {
+    // 6. click on refund policy
     logger("Click on refundsPolicy");
     await driver
       .wait(until.elementLocated(By.css("#dealsDetail-refundpolicy-0")))
       .click();
-  } catch (error) {
-    logger(`error: ${error}`);
-    // await driver.quit();
-    // process.exit(0);
-  }
-
-  // sign-in page
-  // 7. email & pw
-
-  // 7.1 manual login
-  try {
+    // 7. login (manual)
     logger("Logging in");
     await driver.wait(until.elementLocated(By.name("email"))).sendKeys(email);
     await driver
       .wait(until.elementLocated(By.name("password")))
       .sendKeys(pw, Key.ENTER);
-  } catch (error) {
-    logger(`error: ${error}`);
-    await driver.quit();
-    process.exit(0);
-  }
-
-  // 7.2 google login
-  //! google does not let automated tools to access the log in modal/page.
-  //! find a workaround.
-  // try {
-  //   logger("logging in via google");
-  //   await driver.wait(until.elementLocated(By.id("signin-apple"))).click();
-  // const google = await driver.wait(
-  //   until.elementLocated(By.css("#signin-google"))
-  // );
-
-  // await driver
-  //   .findElement({ id: "google" })
-  //   .then((element) => element.click())
-  //   .catch((err) => console.log("nope", err));
-  // } catch (error) {
-  //   logger(`error: ${error}`);
-  //   console.log({ error });
-  // }
-
-  // 8. click on refund policy again (after logging in)
-  try {
+    // 8. click on refund policy again (after logging in)
     logger("Click on refundsPolicy (2nd time)");
     await driver
       .wait(until.elementLocated(By.css("#dealsDetail-refundpolicy-0")))
       .click();
-  } catch (error) {
-    logger(`error: ${error}`);
-    // await driver.quit();
-    // process.exit(0);
-  }
-
-  // booking page, make a reservation.
-  // 9. pick a date and continue
-  try {
+    // 9. pick a date and continue
     logger("Click on date input");
     await driver
       .wait(until.elementLocated(By.css("#date")))
@@ -119,12 +61,7 @@ const run = async () => {
     await driver
       .wait(until.elementLocated(By.css("#booking-continue-btn")))
       .click();
-  } catch (error) {
-    logger(`error: ${error}`);
-  }
-
-  // 10. fill out / change other inputs and submit
-  try {
+    // 10. fill out / change other inputs and submit
     logger("submit the form");
     await driver
       .wait(until.elementLocated(By.css("#booking-userInfo-continue-btn")))
@@ -132,7 +69,12 @@ const run = async () => {
     logger("booking test complete");
   } catch (error) {
     logger(`error: ${error}`);
+
+    await driver.quit();
+    process.exit(0);
   }
+
+  // booking page, make a reservation.
 
   // finishing up
   fs.writeFile("test_logs.txt", testLog, (err) => {
@@ -140,13 +82,7 @@ const run = async () => {
     if (err) {
       console.error(err);
     }
-    // file written successfully
   });
-
-  // await setTimeout(async () => {
-  //   await driver.quit();
-  //   process.exit(0);
-  // }, 2000);
 };
 
 run();
